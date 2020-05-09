182d06cf5449ddab416b81eaa33edcb99ac04f70
Alexey <alexey.beley@gmail.com>
2020-05-09 20:23:59 +0300

    sudo

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,8 +1,4 @@
 apt-get update
 sudo apt install docker.io -y
-
 sudo docker swarm init
-
-sudo docker network create -d web_network \
---driver overlay \
---subnet=192.168.100.0/24
+sudo docker network create -d overlay web_network --subnet=192.168.100.0/24
