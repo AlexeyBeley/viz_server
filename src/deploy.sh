dfeec687ec6eee011ce57bdeb776ca1e13eab622
Alexey <alexey.beley@gmail.com>
2020-05-09 20:35:49 +0300

    fix deploy

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,4 +1,4 @@
 apt-get update
 sudo apt install docker.io -y
 sudo docker swarm init
-sudo docker network create -d overlay web_network --subnet=192.168.100.0/24
+sudo docker network create -d overlay --attachable --subnet=192.168.100.0/24 internal_network
