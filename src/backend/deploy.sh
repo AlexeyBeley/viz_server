d2626c025667344e82119fb0cb08cbc10414479e
Alexey <alexey.beley@gmail.com>
2020-05-09 23:41:29 +0300

    remove exposed port

--- src/backend/deploy.sh
+++ src/backend/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo docker build -t backend .
-sudo docker run -d -p 8000:8000 --network internal_network --ip 192.168.1.2 backend
+sudo docker run -d --network internal_network --ip 192.168.1.2 backend
