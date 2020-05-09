1e39158d8c0ed1c84d464cc8f09977c032c89c6d
Alexey <alexey.beley@gmail.com>
2020-05-09 21:38:56 +0300

    fix nginx

--- src/front/deploy.sh
+++ src/front/deploy.sh
@@ -5,6 +5,6 @@ sudo docker run -d -p 80:80 --network internal_network --ip 192.168.1.3 front
 
 
 #apt-get update
-#apt-get install curl
+#apt-get install curl -y
 #curl http://127.0.0.1/abracadabra
 #curl -i -H 'Content-Type: application/json' http://192.168.1.2:8000/abracadabra
